a6f34fc20e7dd10f91774ed26067a598298e32d2
Jason J Cruz <jaycru06@yahoo.com>
2021-04-28 22:09:40 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,15 @@
 # Use this space to code along with the readme
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
 
 phrase = "Hello World!"
 puts phrase
@@ -11,3 +22,106 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+ 
+# ... The rest of the program
+ 
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+ 
+say_hello_world_ten_times
+ 
+# ... The rest of the program
+ 
+say_hello_world_ten_times
+
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
+
+def greeting
+  puts "Hello World"
+end
+
+greeting
+
+def greeting # Method Signature
+  puts "Hello World" # Method Body
+end # Method Closing
+
+def greeting
+  puts "Hello World" # Now code the body of the method.
+end
+
+def greeting
+  puts "Hello World"
+end
+ 
+greeting # Executing the method by name
+#=> "Hello World"
+ 
+greeting # Executing the method again
+#=> "Hello World"
+
+def greeting
+  puts "Hello World"
+end
+
+def greeting
+  puts "Hello World"
+end
+ 
+greeting
+greeting
+greeting
+greeting
+greeting
+
